c7d57e05d835477c08b116934c32578f318c8cc5
minakshitomar <tomar.minakshi@outlook.com>
2022-09-05 14:23:06 +0530

    min-max sum

---
+++ day5.c
@@ -0,0 +1,25 @@
+// min-max sum
+#include<stdio.h>
+int main()
+{
+    long int a[5], i, min, max,  sum=0;
+    for(i=0; i<5; i++)
+    {
+      scanf("%ld", &a[i]);
+      sum = sum + a[i];       
+    }
+    min = a[0];
+    for(i=0; i<5; i++)
+    {
+        if(min> a[i])
+        min = a[i];
+    }
+    max = a[0];
+    for(i=1; i<5; i++)
+    {
+        if(max < a[i])
+        max= a[i];
+    }
+    printf("%ld %ld ", sum - max, sum - min);
+    return 0;
+}
