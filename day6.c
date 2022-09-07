05b862ae6a4392956e851402b6ae98483f556718
minakshitomar <tomar.minakshi@outlook.com>
2022-09-07 13:59:00 +0530

    compare the triplets

---
+++ day6.c
@@ -0,0 +1,128 @@
+//time conversion by help 
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* timeConversion(char* s) 
+{
+    int hr = atoi (strtok(s, ":"));
+    int min = atoi (strtok(NULL, ":"));
+    int sec = atoi (strtok(NULL, ""));
+    char ampm[3];
+    ampm[0]=s[8];
+    ampm[1]='M';
+    char* time = malloc(128 * sizeof(char));
+    if(strcmp(ampm,"AM")==0 && hr==12)
+    sprintf(time, "00:%.2d:%.2d", min, sec);
+    else if(strcmp(ampm,"AM")==0)
+    sprintf(time, "%.2d:%.2d:%.2d",hr, min, sec);
+    else if(strcmp(ampm,"PM")==0)
+    {
+        if (hr!=12)
+        hr = hr +12;
+        sprintf(time,"%.2d:%.2d:%.2d",hr, min, sec);
+    }
+    return time;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
